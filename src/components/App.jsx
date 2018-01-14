e7ae3d4fa8fa219d2c558aa1d46755351c974c21
Pranay Pradhananga <pronoy19114@gmail.com>
2018-01-15 09:49:33 +1100

    changes to app component to test react loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.appList = ["word", "cross", "jumble", "new"]
+    }
+
+    renderList() {
+        return this.appList.map((app,i)=><li key={i}>{app}</li>)
+    }
     render() {
-        return <div> Hello World </div>;
+        return <ul>{this.renderList()}</ul>;
     }
 }
